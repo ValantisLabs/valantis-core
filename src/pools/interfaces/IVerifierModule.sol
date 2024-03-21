6c95a402da33316a89aa8f145822e6ee782324f0
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-03-21 15:46:42 +0530

    chore: natspec for swap callback interfaces

--- src/pools/interfaces/IVerifierModule.sol
+++ src/pools/interfaces/IVerifierModule.sol
@@ -6,9 +6,9 @@ interface IVerifierModule {
      * @notice Used to verify user access to important pool functions.
      * @param _user The address of the user.
      * @param _verificationContext Arbitrary bytes data which can be sent to the verifier module.
-     * @param accessType The type of function being called, can be - SWAP(0), DEPOSIT(1), or WITHDRAW(2)
-     * @return success True if the user is verified, false otherwise
-     * @return returnData Additional data which can be passed along to the LM in case of a swap
+     * @param accessType The type of function being called, can be - SWAP(0), DEPOSIT(1), or WITHDRAW(2).
+     * @return success True if the user is verified, false otherwise.
+     * @return returnData Additional data which can be passed along to the LM in case of a swap.
      */
     function verify(
         address _user,
