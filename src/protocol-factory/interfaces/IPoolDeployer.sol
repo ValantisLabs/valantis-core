ddffedc401b90ec6cd18467749c1583b946800dc
Tanishk Goyal <goyaltanishk02@gmail.com>
2024-03-21 16:00:54 +0530

    chore: natspec for swap fee module and pool deployer functions

--- src/protocol-factory/interfaces/IPoolDeployer.sol
+++ src/protocol-factory/interfaces/IPoolDeployer.sol
@@ -2,5 +2,11 @@
 pragma solidity 0.8.19;
 
 interface IPoolDeployer {
+    /**
+     * @notice Deploys a new pool.
+     * @param _salt The salt to use for deployment.
+     * @param _constructorArgs The arguments to pass to the pool constructor.
+     * @return deployment The address of the deployed pool.
+     */
     function deploy(bytes32 _salt, bytes calldata _constructorArgs) external returns (address deployment);
 }
